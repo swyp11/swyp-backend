ea445a7e9a40a4db13df0d8ee034ee7bde736073
bukwon <dnu05043@gmail.com>
2025-10-19 14:57:08 +0900

    feat: init

---
+++ src/test/java/com/swyp/wedding/WeddingInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swyp.wedding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
