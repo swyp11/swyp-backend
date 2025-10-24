4f469195dffd754b0b42b4feb39780041b90009e
h.zoo <h.zoo3963@gmail.com>
2025-10-24 17:36:24 +0900

    feat:회원가입 기능 구현 및 securityConfig 구현

---
+++ src/main/java/com/swyp/wedding/controller/JoinController.java
@@ -0,0 +1,20 @@
+package com.swyp.wedding.controller;
+
+import com.swyp.wedding.dto.user.UserRequest;
+import com.swyp.wedding.service.JoinService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class JoinController {
+
+    private final JoinService joinService;
+
+    @PostMapping("/join")
+    public String JoinProcess(@RequestBody UserRequest userRequest){
+        return joinService.JoinProcess(userRequest);
+    }
+}
--- src/main/java/com/swyp/wedding/repository/user/UserRepository.java
+++ src/main/java/com/swyp/wedding/repository/user/UserRepository.java
@@ -1,10 +1,12 @@
 package com.swyp.wedding.repository.user;
 
+import com.swyp.wedding.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.swyp.wedding.entity.user.User;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Boolean existsByUserId(String userId);
+    Boolean existsByPhoneNumber(String phoneNumber);
 }
