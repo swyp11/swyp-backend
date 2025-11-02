dd1a372a10be30cd41e68d6e376b89142414fd3e
suzy kang <szkang@hancomspace.com>
2025-11-02 17:23:23 +0900

    feat: dress dto 추가, enum추가, api추가

---
+++ src/main/java/com/swyp/wedding/dto/dress/DressRequest.java
@@ -0,0 +1,42 @@
+package com.swyp.wedding.dto.dress;
+
+import lombok.Data;
+import com.swyp.wedding.entity.dress.Dress;
+import com.swyp.wedding.entity.dress.DressEnum;
+
+@Data
+public class DressRequest {
+    private Long id;
+    private String name;      
+    private String color;           
+    private String shape;           
+    private String priceRange;      
+    private DressEnum.Length length;        
+    private DressEnum.Season season;   
+    private String brand;           
+    private String designer;        
+    private DressEnum.Type type;            
+    private DressEnum.Neckline neckLine;     
+    private DressEnum.Mood mood;              
+    private String fabric;  // 여러 원단 타입을 콤마로 구분 (예: "LACE,BEADS")
+    private String usage;   // 여러 용도를 콤마로 구분 (예: "MAIN_CEREMONY,PHOTO_SHOOT")
+    private String features;  // 특징 또는 기타 컬럼            
+
+    public Dress toEntity() {
+        return Dress.builder()
+                .name(name)                 
+                .color(color)
+                .shape(shape)
+                .priceRange(priceRange)
+                .length(length)
+                .season(season)             
+                .brand(brand)
+                .designer(designer)
+                .type(type)              
+                .neckLine(neckLine)       
+                .mood(mood)             
+                .fabric(fabric)
+                .features(features)         
+                .build();
+    }
+}
