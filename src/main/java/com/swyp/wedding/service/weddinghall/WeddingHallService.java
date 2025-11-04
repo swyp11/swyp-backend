ca5d8db14d46dc98e3d21bb3061fa543e61453f9
bukwon <dnu05043@gmail.com>
2025-11-04 22:45:04 +0900

    fix: 수정 및 저장 기능 수정

--- src/main/java/com/swyp/wedding/service/weddinghall/WeddingHallService.java
+++ src/main/java/com/swyp/wedding/service/weddinghall/WeddingHallService.java
@@ -1,15 +1,13 @@
 package com.swyp.wedding.service.weddinghall;
 
-import com.swyp.wedding.dto.weddinghall.WeddingHallRequest;
-import com.swyp.wedding.dto.weddinghall.WeddingHallResponse;
-import com.swyp.wedding.entity.weddinghall.WeddingHall;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
-@Service
+import com.swyp.wedding.dto.weddinghall.WeddingHallRequest;
+import com.swyp.wedding.dto.weddinghall.WeddingHallResponse;
+
 public interface WeddingHallService {
     List<WeddingHallResponse> getWeddingInfos();
-    WeddingHall saveWedding(WeddingHallRequest request);
+    boolean saveWedding(WeddingHallRequest request);
     WeddingHallResponse getWeddingInfo(Long id);
+    boolean updateWedding(WeddingHallRequest request);
 }
