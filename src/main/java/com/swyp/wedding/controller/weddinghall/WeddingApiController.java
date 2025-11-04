376c9e84ee3de698356cedc287e4bbce65bb3d37
bukwon <dnu05043@gmail.com>
2025-11-04 22:45:04 +0900

    fix: 수정 및 저장 기능 수정

--- src/main/java/com/swyp/wedding/controller/weddinghall/WeddingApiController.java
+++ src/main/java/com/swyp/wedding/controller/weddinghall/WeddingApiController.java
@@ -1,15 +1,23 @@
 package com.swyp.wedding.controller.weddinghall;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.swyp.wedding.dto.weddinghall.WeddingHallRequest;
 import com.swyp.wedding.dto.weddinghall.WeddingHallResponse;
 import com.swyp.wedding.service.weddinghall.WeddingHallServiceImpl;
+
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -34,7 +42,13 @@ public class WeddingApiController {
     @Operation(summary = "웨딩홀에 대한 정보를 저장합니다.")
     @PostMapping
     public ResponseEntity<Map<String, Object>> saveWedding(@RequestBody WeddingHallRequest request) {
-        return ResponseEntity.ok(Map.of("result", weddingHallService.saveWedding(request) != null));
+        return ResponseEntity.ok(Map.of("result", weddingHallService.saveWedding(request)));
     }
 
+    @Operation(summary = "웨딩홀에 대한 정보를 수정합니다.")
+    @PutMapping("/{id}")
+    public ResponseEntity<Map<String, Object>> updateWedding(@PathVariable Long id, @RequestBody WeddingHallRequest request) {
+        request.setId(id);
+        return ResponseEntity.ok(Map.of("result", weddingHallService.updateWedding(request)));
+    }
 }
