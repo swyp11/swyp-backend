d6b8628748c108ca4edf24c1a421909ad1c47c33
h.zoo <h.zoo3963@gmail.com>
2025-11-05 23:47:46 +0900

    chore: join 관련 폴더 이동

--- src/main/java/com/swyp/wedding/controller/login/Oauth2TestController.java
+++ src/main/java/com/swyp/wedding/controller/login/Oauth2TestController.java
@@ -1,4 +1,4 @@
-package com.swyp.wedding.controller;
+package com.swyp.wedding.controller.login;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
