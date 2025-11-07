bf32809c28a81f3badcba4fbafdb36339b6d630d
suzy kang <szkang@hancomspace.com>
2025-11-07 20:42:41 +0900

    fix: 드레스 컬럼 수정 및 api 수정

--- src/main/java/com/swyp/wedding/repository/dress/DressRepository.java
+++ src/main/java/com/swyp/wedding/repository/dress/DressRepository.java
@@ -1,10 +1,20 @@
 package com.swyp.wedding.repository.dress;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.swyp.wedding.entity.dress.Dress;
 
+import java.util.List;
+
 @Repository
 public interface DressRepository extends JpaRepository<Dress, Long> {
+    
+    // shopName으로 드레스들 조회
+    List<Dress> findByShopName(String shopName);
+    
+    // shopName으로 드레스들 조회 (부분 일치)
+    List<Dress> findByShopNameContainingIgnoreCase(String shopName);
 }
