f4dff07d6d95a7c8664210ada4320c42718ac021
suzy kang <szkang@hancomspace.com>
2025-11-07 20:43:07 +0900

    feat: dress shop 추가

---
+++ src/main/java/com/swyp/wedding/repository/dressshop/DressShopRepository.java
@@ -0,0 +1,22 @@
+package com.swyp.wedding.repository.dressshop;
+
+import com.swyp.wedding.entity.dressshop.DressShop;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DressShopRepository extends JpaRepository<DressShop, Long> {
+    
+    // 샵 이름으로 검색
+    List<DressShop> findByShopNameContainingIgnoreCase(String shopName);
+    
+    // 지역(주소)으로 검색
+    List<DressShop> findByAddressContainingIgnoreCase(String address);
+    
+    // 전문분야로 검색
+    List<DressShop> findBySpecialtyContainingIgnoreCase(String specialty);
+}
