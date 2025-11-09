2bab9350bcde7a5d01153793096b76c011f103a2
h.zoo <h.zoo3963@gmail.com>
2025-11-09 22:09:05 +0900

    refactor: oAuth 로그인 시 userId 값 변경 및 관련 로직

--- src/main/java/com/swyp/wedding/controller/user/AuthController.java
+++ src/main/java/com/swyp/wedding/controller/user/AuthController.java
@@ -21,7 +21,7 @@ public class AuthController {
 
     @PostMapping("/oauth/login/{provider}")
     @Operation(summary = "OAuth 로그인" ,description = "provider에는 현재 google만 존재 이후 추가 가능성 대비")
-    public ResponseEntity<TokenResponse> googleLogin(@PathVariable String provider,
+    public ResponseEntity<TokenResponse> OAuthLogin(@PathVariable String provider,
                                                      @RequestBody OAuthCodeRequest auth){
         TokenResponse jwtToken = authService.processOAuthLogin(provider, auth.getCode(), auth.getRedirectUri());
         return ResponseEntity.ok(jwtToken);
@@ -29,7 +29,7 @@ public class AuthController {
 
     // 일반 로그인
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<?> Login(@RequestBody LoginRequest loginRequest) {
         TokenResponse tokenResponse = authService.login(loginRequest);
         return ResponseEntity.ok()
                 .header("Authorization", "Bearer " + tokenResponse.getAccessToken())
