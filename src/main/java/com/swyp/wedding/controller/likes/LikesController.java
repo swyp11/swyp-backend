0944ed27d14eca7a5136a2fc1d38fc5fa3500717
Ahnyeongjun <toinin@naver.com>
2025-11-12 21:19:19 +0900

    fix: principal CustomUserDetails로 업데이트

--- src/main/java/com/swyp/wedding/controller/likes/LikesController.java
+++ src/main/java/com/swyp/wedding/controller/likes/LikesController.java
@@ -1,6 +1,7 @@
 package com.swyp.wedding.controller.likes;
 
 import com.swyp.wedding.global.response.ApiResponse;
+import com.swyp.wedding.security.user.CustomUserDetails;
 import com.swyp.wedding.service.likes.impl.LikesServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -8,8 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.security.Principal;
 
 @Tag(name = "찜", description = "찜 기능 관리 API")
 @RequiredArgsConstructor
@@ -22,8 +21,8 @@ public class LikesController {
     @Operation(summary = "사용자가 특정 게시물 좋아요(찜)를 누릅니다.")
     @PostMapping("/{category}/{postId}")
     public ResponseEntity<ApiResponse<Void>> storeLikes(@PathVariable String category, @PathVariable Long postId,
-                                                          @AuthenticationPrincipal Principal principal) {
-        String userId = principal.getName();
+                                                          @AuthenticationPrincipal CustomUserDetails userDetails) {
+        String userId = userDetails.getUsername();
         likesService.storeLikes(category, postId, userId);
         return ResponseEntity.ok(ApiResponse.success());
     }
@@ -31,7 +30,7 @@ public class LikesController {
     @Operation(summary = "사용자가 특정 게시물 좋아요(찜)를 제거합니다.")
     @DeleteMapping("/{id}")
     public ResponseEntity<ApiResponse<Void>> deleteLikes(@PathVariable Long id,
-                                                          @AuthenticationPrincipal Principal principal) {
+                                                          @AuthenticationPrincipal CustomUserDetails userDetails) {
         // 추후 계정 없는 것에 대한 예외처리 기능 만들 예정 (고도화 때)
         likesService.deleteLikes(id);
         return ResponseEntity.ok(ApiResponse.success());
